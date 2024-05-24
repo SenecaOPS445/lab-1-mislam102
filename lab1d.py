90f78ecd0c5bdea6a699c4b096eca55d13116d12
Mahmudul Islam <mislam102@myseneca.ca>
2024-05-24 19:26:49 -0400

    Added lab1d.py.

---
+++ lab1d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Define the integer values
+x = 10
+y = 2
+z = 5
+
+#calculation
+result = x + y * z
+
+# output
+print(str(x) + " + " + str(y) + " * " + str(z) + " = " + str(result))
